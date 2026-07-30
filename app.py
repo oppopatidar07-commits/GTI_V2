e56230fbc61c847446632dfe9eca659161527bd6
oppopatidar07-commits <oppopatidar07@gmail.com>
2026-07-30 13:13:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, jsonify
 
+from constants import PROJECT_NAME
+from strategy import get_signal
+from market_data import get_market_status
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return jsonify({
-        "project": "GTI Verification",
+        "project": PROJECT_NAME,
         "status": "Running",
-        "step": "Phase 1"
+        "market": get_market_status(),
+        "signal": get_signal()
     })
+
 
 @app.route("/health")
 def health():
-    return jsonify({"status": "OK"})
+    return jsonify({
+        "status": "OK"
+    })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
