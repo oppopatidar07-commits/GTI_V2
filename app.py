f029d55947b27dd8911d73d1d659bd6951832acb
oppopatidar07-commits <oppopatidar07@gmail.com>
2026-07-30 12:32:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "GTI Verification Server Running Successfully!"
+    return {
+        "status": "GTI Server Running",
+        "api_key_found": bool(os.getenv("API_KEY")),
+        "client_code_found": bool(os.getenv("CLIENT_CODE")),
+        "pin_found": bool(os.getenv("PIN")),
+        "totp_secret_found": bool(os.getenv("TOTP_SECRET"))
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
