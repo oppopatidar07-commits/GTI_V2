375d2a429f2c7eb0514b67b08421803d5b6c0eff
oppopatidar07-commits <oppopatidar07@gmail.com>
2026-07-30 13:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return jsonify({
@@ -11,13 +10,9 @@ def home():
         "step": "Phase 1"
     })
 
-
 @app.route("/health")
 def health():
-    return jsonify({
-        "status": "OK"
-    })
-
+    return jsonify({"status": "OK"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
